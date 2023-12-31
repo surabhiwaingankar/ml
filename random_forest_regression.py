b9847075580061a73387756a1e7a2e58efd03a84
Surabhi Waingankar <surabhiwaingankar@gmail.com>
2023-12-31 12:21:45 +0530

    Add files via upload

---
+++ random_forest_regression.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 31 11:35:10 2023
+
+@author: Lenovo
+"""
+
+#Random Forest Regression
+
+#Data preprocessing
+
+#importing libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,-1].values
+
+"""
+#splitting the dataset into training and testing sets
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
+
+
+feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y.reshape(-1, 1))
+
+"""
+#Fitting Random Forest Regression to the Dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=300, random_state=0)
+regressor.fit(X,y)
+
+
+#Visualizing Random Forest Regression results(for higher order resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title("Truth or Bluff (Random Forest Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+input_value = 6.5
+input_array = np.array(input_value).reshape(1, -1)
+y_pred = regressor.predict(input_array)
